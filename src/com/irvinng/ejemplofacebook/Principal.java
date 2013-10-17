8f8b6a38d71f9815de612545be0ed88f9db8d133
kodamirmo <irvingemmanuelgg@gmail.com>
2013-10-17 11:13:20 -0500

    Facebook example first commit

---
+++ src/com/irvinng/ejemplofacebook/Principal.java
@@ -0,0 +1,84 @@
+package com.irvinng.ejemplofacebook;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.pm.Signature;
+import android.util.Base64;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.facebook.*;
+import com.facebook.model.*;
+
+public class Principal extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_principal);
+
+		printHash();
+
+		Session.openActiveSession(this, true, new Session.StatusCallback() {
+
+			// callback when session changes state
+			@Override
+			public void call(Session session, SessionState state,
+					Exception exception) {
+				if (session.isOpened()) {
+
+					// make request to the /me API
+					Request.executeMeRequestAsync(session,
+							new Request.GraphUserCallback() {
+
+								// callback after Graph API response with user
+								// object
+								@Override
+								public void onCompleted(GraphUser user,
+										Response response) {
+									if (user != null) {
+										TextView welcome = (TextView) findViewById(R.id.tvHolaFacebook);
+										welcome.setText("Hello "
+												+ user.getName() + "!");
+									}
+								}
+							});
+				}
+			}
+		});
+
+	}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		Session.getActiveSession().onActivityResult(this, requestCode,
+				resultCode, data);
+	}
+
+	private void printHash() {
+		try {
+			PackageInfo info = getPackageManager().getPackageInfo(
+					"com.irvinng.ejemplofacebook",
+					PackageManager.GET_SIGNATURES);
+			for (Signature signature : info.signatures) {
+				MessageDigest md = MessageDigest.getInstance("SHA");
+				md.update(signature.toByteArray());
+				Log.e("KeyHash:",
+						Base64.encodeToString(md.digest(), Base64.DEFAULT));
+			}
+		} catch (NameNotFoundException e) {
+			e.printStackTrace();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
